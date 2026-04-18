3a4ccc823f3f5d2256e142efa0acbb9366dfb01b
ABHIRAM_S <149877367+Abhiram-ARS@users.noreply.github.com>
2026-04-18 21:13:10 +0530

    Add files via upload

---
+++ code/BackEnd/Certificate.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract CertificateRegistry {
+
+    address public owner;
+
+    struct Certificate {
+        uint256 issuedAt;
+        bool exists;
+        bool revoked;
+        uint256 revokedAt;
+    }
+
+    mapping(bytes32 => Certificate) private certificates;
+
+    event CertificateIssued(bytes32 certHash, uint256 issuedAt);
+    event CertificateRevoked(bytes32 certHash, uint256 revokedAt);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not authorized");
+        _;
+    }
+
+    function issueCertificate(bytes32 certHash) public onlyOwner {
+
+        require(!certificates[certHash].exists, "Certificate already exists");
+
+        certificates[certHash] = Certificate(
+            block.timestamp,
+            true,
+            false,
+            0
+        );
+
+        emit CertificateIssued(certHash, block.timestamp);
+    }
+
+    function revokeCertificate(bytes32 certHash) public onlyOwner {
+
+        require(certificates[certHash].exists, "Certificate not found");
+        require(!certificates[certHash].revoked, "Already revoked");
+
+        certificates[certHash].revoked = true;
+        certificates[certHash].revokedAt = block.timestamp;
+
+        emit CertificateRevoked(certHash, block.timestamp);
+    }
+
+    function verifyCertificate(bytes32 certHash)
+        public
+        view
+        returns (
+            bool exists,
+            bool revoked,
+            uint256 issuedAt,
+            uint256 revokedAt
+        )
+    {
+        Certificate memory cert = certificates[certHash];
+
+        return (
+            cert.exists,
+            cert.revoked,
+            cert.issuedAt,
+            cert.revokedAt
+        );
+    }
+}
